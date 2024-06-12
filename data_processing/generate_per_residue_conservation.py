7b78a0d4cf9e44817162cc1c7f043d5c58ba78e5
meconsens <micaela.consens@mail.utoronto.ca>
2024-06-12 14:05:40 +0000

    update generate_per_residue_conservation to make two fasta files

--- data_processing/generate_per_residue_conservation.py
+++ data_processing/generate_per_residue_conservation.py
@@ -1,63 +1,99 @@
 import pyBigWig
 import pandas as pd
-import os 
+import os
 import argparse
+from pyfaidx import Fasta
 
-#generates a dataframe with per nucleotide conservation scores
-def make_scores_df(bigwig_file, bed, file_path):
-    #open the bigwig file
+
+# create two fasta files in the specified directory: sequences.fasta and conservation_scores.fasta
+# sequences.fasta file will contain the sequences from the genome corresponding to the regions specified in the BED file
+# conservation_scores.fasta file will contain the conservation scores for these regions
+# fasta files will have a header in the format >chrom:start-end, followed by the sequence or conservation scores
+def make_scores_df(bigwig_file, bed, file_path, genome_fasta):
+    # open the bigwig file
     bw = pyBigWig.open(bigwig_file)
 
-    #empty list to store conservation scores
+    # open the genome fasta file
+    genome = Fasta(genome_fasta)
+
+    # empty list to store conservation scores
     conservation_scores = []
 
-    #iterate over the BED file
-    for index, row in bed.iterrows():
-        #get the conservation scores
-        chrom = row["chrom"]
-        start = row["start"]
-        end = row["end"]
+    # open the output fasta files
+    with open(f"{file_path}sequences.fasta", "w") as seq_fasta, open(
+        f"{file_path}conservation_scores.fasta", "w"
+    ) as cons_fasta:
+        # iterate over the BED file
+        for index, row in bed.iterrows():
+            # get the conservation scores
+            chrom = row["chrom"]
+            start = row["start"]
+            end = row["end"]
 
-        #get scores for each position in the range
-        vals = bw.values(chrom, start, end, numpy = True)
+            # get scores for each position in the range
+            vals = bw.values(chrom, start, end, numpy=True)
 
-        #check if the returned vals are valid
-        if vals is not None:
-            #replace nans with 0s
-            vals = [0 if pd.isna else val for val in vals]
+            # check if the returned vals are valid
+            if vals is not None:
+                # replace nans with 0s
+                vals = [0 if pd.isna(val) else val for val in vals]
+            else:
+                # if the returned vals are invalid, append 0s
+                vals = [0] * (end - start)
+
             conservation_scores.append(vals)
-        else:
-            #if the returned vals are invalid,append 0s
-            conservation_scores.append([0] * (end - start))
-        
-    #close the bigwig file
+
+            # get the sequence from the genome
+            sequence = genome[chrom][start:end].seq
+
+            # write the sequence to the fasta file
+            seq_fasta.write(f">{chrom}:{start}-{end}\n{sequence}\n")
+
+            # write the conservation scores to the fasta file
+            cons_fasta.write(f">{chrom}:{start}-{end}\n{''.join(map(str, vals))}\n")
+
+    # close the bigwig file
     bw.close()
 
-    #flatten the scores and create new df for the conservation scores
-    flattened_scores = []
-    for index, score_list in enumerate(conservation_scores):
-        for position, score in enumerate(score_list):
-            flattened_scores.append([bed.loc[index, "chrom"], bed.loc[index, "start"] + position, bed.loc[index, "start"] + position +1, score])
-
-    flattened_scores_df = pd.DataFrame(flattened_scores, columns=["chrom", "start", "end", "conservation_score"])
-
-    #save df to new BED
-    flattened_scores_df.to_csv(f'{file_path}conservation_scores.bed', sep="\t", index=False, header=False)
 
 def main():
-    #process command line arguments
-    parser = argparse.ArgumentParser(description='Generate a dataframe with per nucleotide conservation scores')
-    parser.add_argument('--bigwig_file', type=str, default='/home/t-mconsens/micaconsenshot/241-mammalian-2020v2.bigwig', help='Path to the bigwig file with phyloP scores')
-    parser.add_argument('--bed_file', type=str, default ='/home/t-mconsens/gamba/data_processing/hg38.bed', help='File name of the bed file')
-    parser.add_argument('--file_path', type=str, default ='/home/t-mconsens/gamba/data_processing/', help='Directory to save the new dataframe')
+    # process command line arguments
+    parser = argparse.ArgumentParser(
+        description="Generate.fasta file for sequences and for conservation scores from a bigwig file and a BED file"
+    )
+    parser.add_argument(
+        "--bigwig_file",
+        type=str,
+        default="/home/t-mconsens/gamba/data_processing/data/241-mammalian-2020v2.bigwig",
+        help="Path to the bigwig file with phyloP scores",
+    )
+    parser.add_argument(
+        "--bed_file",
+        type=str,
+        default="/home/t-mconsens/gamba/data_processing/data/sequences_human.bed",
+        help="File name of the bed file",
+    )
+    parser.add_argument(
+        "--file_path",
+        type=str,
+        default="/home/t-mconsens/gamba/data_processing/data/",
+        help="Directory to save the new sequence and conservation scores fasta",
+    )
+    parser.add_argument(
+        "--genome_fasta",
+        type=str,
+        default="/home/t-mconsens/gamba/data_processing/data/hg38.fa",
+        help="Path to the genome fasta file",
+    )
     args = parser.parse_args()
 
-    #load the BED file to pandas df
-    bed = pd.read_csv(args.bed_file, sep="\t", header=None, names=["chrom", "start", "end"])
+    # load the BED file to pandas df
+    bed = pd.read_csv(
+        args.bed_file, sep="\t", header=None, names=["chrom", "start", "end"]
+    )
 
-    make_scores_df(args.bigwig_file, bed, args.file_path)
-    print(f"Conservation scores BED file created: {args.file_path}conservation_scores.bed")
-
+    make_scores_df(args.bigwig_file, bed, args.file_path, args.genome_fasta)
+    print(f"Sequences and conservation scores fasta files created in: {args.file_path}")
 
 
 if __name__ == "__main__":
